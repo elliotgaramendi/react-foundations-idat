c1188c1bc8c45d70c6983cd51593fc9697418887
elliotgaramendi <elliotgaramendi@gmail.com>
2025-04-16 21:40:34 -0500

    feat: update ProfessionalCard component to accept props for name, age, and isAlive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import './App.css'
-import reactLogo from './assets/react.svg'
-import ProfessionalCard from './components/ProfessionalCard'
+import './App.css';
+import reactLogo from './assets/react.svg';
+import ProfessionalCard from './components/ProfessionalCard';
 
 function App() {
+  const person1 = {
+    name: 'Elliot Garamendi',
+    age: 29,
+    isAlive: true,
+    roles: ['🌌Frontend🌌', '🤖AI Engineer🤖', '👨‍🏫Docente👨‍🏫'],
+    skills: ['⚛️ React ⚛️', '🚀 Astro 🚀', '🖌️ Figma 🖌️', '🤖 OpenAI 🤖'],
+    address: { country: '🇵🇪 Perú 🇵🇪', city: 'Lima' },
+  };
+
+  const person2 = {
+    name: 'Mijael Garamendi',
+    age: 2,
+    isAlive: true,
+    roles: ['🌌Frontend🌌', '🤖AI Engineer🤖', '👨‍🏫Docente👨‍🏫'],
+    skills: ['⚛️ React ⚛️', '🚀 Astro 🚀', '🖌️ Figma 🖌️', '🤖 OpenAI 🤖'],
+    address: { country: '🇵🇪 Perú 🇵🇪', city: 'Lima' },
+  };
 
   return (
     <>
@@ -10,7 +27,8 @@ function App() {
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
       <h1>React Foundation</h1>
-      <ProfessionalCard />
+      <ProfessionalCard name={person1.name} age={person1.age} isAlive={person1.isAlive} />
+      <ProfessionalCard name={person2.name} age={person2.age} isAlive={person2.isAlive} />
     </>
   )
 }
--- src/components/ProfessionalCard.jsx
+++ src/components/ProfessionalCard.jsx
@@ -1,23 +1,17 @@
 import { useState } from 'react';
 
-const ProfessionalCard = () => {
+const ProfessionalCard = ({ name, age, isAlive }) => {
+  console.log(name);
   const [like, setLike] = useState(0);
-
-  const name = 'Elliot Garamendi';
-  const age = 29;
-  const isAlive = true;
-  const roles = ['🌌Frontend🌌', '🤖AI Engineer🤖', '👨‍🏫Docente👨‍🏫']
-  const skills = ['⚛️ React ⚛️', '🚀 Astro 🚀', '🖌️ Figma 🖌️', '🤖 OpenAI 🤖'];
-  const address = { country: '🇵🇪 Perú 🇵🇪', city: 'Lima' };
-
   const handleLike = () => setLike(like + 1);
 
   return (
     <div className="card">
       <h2>♥️ {name} <sup>{age}</sup> <sub>{isAlive ? '🟢' : '🔴'}</sub>♥️</h2>
+      {/* 
       <h3>{address.country}, {address.city}</h3>
       <h4>{roles.join(' I ')}</h4>
-      <h5>{skills.join(', ')}</h5>
+      <h5>{skills.join(', ')}</h5> */}
       <button onClick={handleLike}>Like ❤️ {like}</button>
     </div>
   );
