2e6d3154d61faec5f05ecf86103022c0333204e7
elliotgaramendi <elliotgaramendi@gmail.com>
2025-04-16 21:13:18 -0500

    feat: add like functionality and simplify roles and skills in ProfessionalCard component

--- src/components/ProfessionalCard.jsx
+++ src/components/ProfessionalCard.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+
 const ProfessionalCard = () => {
+  const [like, setLike] = useState(0);
+
   const name = 'Elliot Garamendi';
   const age = 29;
   const isAlive = true;
-  const roles = ['🌌Frontend🌌', '🤖AI Engineer🤖', '💻Consultor💻', '👨‍🏫Docente👨‍🏫']
-  const skills = ['⚛️ React ⚛️', '🌐 Next.js 🌐', '🚀 Astro 🚀', '🤖 OpenAI 🤖', '🐳 Docker 🐳', '🎨 UI/UX 🎨', '🖌️ Figma 🖌️', '🧪 Testing 🧪'];
+  const roles = ['🌌Frontend🌌', '🤖AI Engineer🤖', '👨‍🏫Docente👨‍🏫']
+  const skills = ['⚛️ React ⚛️', '🚀 Astro 🚀', '🖌️ Figma 🖌️', '🤖 OpenAI 🤖'];
   const address = { country: '🇵🇪 Perú 🇵🇪', city: 'Lima' };
+
+  const handleLike = () => setLike(like + 1);
 
   return (
     <div className="card">
@@ -12,6 +18,7 @@ const ProfessionalCard = () => {
       <h3>{address.country}, {address.city}</h3>
       <h4>{roles.join(' I ')}</h4>
       <h5>{skills.join(', ')}</h5>
+      <button onClick={handleLike}>Like ❤️ {like}</button>
     </div>
   );
 };
